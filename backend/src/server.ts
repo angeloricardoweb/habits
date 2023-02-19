9f7bb9cf816e18240cfb40cfc3b2adba582be7cc
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-19 14:47:28 -0300

    refactor: separete routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,10 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 const app = Fastify();
 
 app.register(cors);
-
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany();
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
