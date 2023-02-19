887f8de3283627cdccbec7d22cfcdb6878ff36c8
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-19 15:02:25 -0300

    dep: add lib dayjs

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "./lib/prisma";
+import { z } from "zod";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const habits = await prisma.habit.findMany();
-    return habits;
+  app.post("/habits", async (request) => {
+    const createHabitSchema = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+    const { title, weekDays } = createHabitSchema.parse(request.body);
   });
 }
