bdd105c37a4d94a9c5713499df96b62b513d0672
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-23 00:12:15 -0200

    chore: config ui lib

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx" ],
+  content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#09090a",
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
-}
+};
