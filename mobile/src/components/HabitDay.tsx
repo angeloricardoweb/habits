6ddc82598d716e1a627067c4e93e454343b67aa4
Angelo Ricardo <angeloengcomp@gmail.com>
2023-02-25 23:06:09 -0300

    ui: add habit buttons

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -10,6 +10,7 @@ export const DAY_SIZE = (Dimensions.get('screen').width / WEEKDAYS) - (SCREEN_HO
 export const HabitDay: FC = () => {
   return (
     <TouchableOpacity className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      activeOpacity={0.7}
       style={{
         width: DAY_SIZE,
         height: DAY_SIZE,
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import { DAY_SIZE, HabitDay } from '../components/HabitDay'
 import { Header } from '../components/Header'
+import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-year-beginning'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const datesFromYearStart = generateDatesFromYearBeginning()
+const minimumSimmaryDatesSizes = 18 * 5
+const amountOfDaysToFill = minimumSimmaryDatesSizes - datesFromYearStart.length
 
 export const Home: FC = () => {
   return (
@@ -24,7 +28,32 @@ export const Home: FC = () => {
           )
         })}
       </View>
-      <HabitDay/>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        <View className='flex-row flex-wrap'>
+          {datesFromYearStart.map((date, index) => {
+            return (
+              <HabitDay key={index} />
+            )
+          })}
+          {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+            return (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                style={{
+                  width: DAY_SIZE,
+                  height: DAY_SIZE,
+                }}
+              />
+            )
+          })}
+        </View>
+      </ScrollView>
     </View>
   )
 }
